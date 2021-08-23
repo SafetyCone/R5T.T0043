3eb2fc5aee9ae915c84cf55a84e5de3a8f504220
David Coats <david@getsafetycone.com>
2021-08-22 20:59:19 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0043.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.T0043.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
